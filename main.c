cd9468a505a681f5733cbf76c247698c437ab922
Evan <evan@MacBook-de-Dark-Evanus.local>
2023-11-22 19:24:33 +0100

    lvl2

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#include <stdlib.h>
+#include <string.h>
+struct Joueur{
+    char nom[40];
+    int score
+};
+void lvl2(){
+    struct Joueur *joueur;
+    printf("Niveau 2");
+    printf("Nom du joueur: %s\n Score: %d\n", joueur->nom, joueur->score);
 }
